f250a33af0166169d2ff4c85d2cf7582e269fde3
Ekaterina Semenova <ek.semenova94@gmail.com>
2026-07-15 11:42:54 +0300

    feat: внедрен кастомный провайдер: доступ к репозиторию производится через интерфейс FilmsRepository

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,6 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Film } from './films/film.entity';
 import { Schedule } from './films/schedule.entity';
 import { FilmsPostgresRepository } from './repository/films.repository';
+import { FILMS_REPOSITORY } from './repository/films.repository.interface';
 
 @Module({
   imports: [
@@ -41,49 +42,10 @@ import { FilmsPostgresRepository } from './repository/films.repository';
     configProvider,
     FilmsService,
     OrderService,
-    FilmsPostgresRepository,
+    {
+      provide: FILMS_REPOSITORY,
+      useClass: FilmsPostgresRepository,
+    },
   ],
 })
 export class AppModule {}
-
-// import { Module } from '@nestjs/common';
-// import { ServeStaticModule } from '@nestjs/serve-static';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-// import * as path from 'node:path';
-// import { configProvider } from './app.config.provider';
-// import { FilmsController } from './films/films.controller';
-// import { FilmsService } from './films/films.service';
-// import { OrderController } from './order/order.controller';
-// import { OrderService } from './order/order.service';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { Film, FilmSchema } from './films/film.schema';
-// import { FilmsMongoDbRepository } from './repository/films.repository';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({
-//       isGlobal: true,
-//       cache: true,
-//     }),
-//     MongooseModule.forRootAsync({
-//       inject: [ConfigService],
-//       useFactory: (configService: ConfigService) => ({
-//         uri: configService.get<string>('DATABASE_URL'),
-//       }),
-//     }),
-//     MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
-//     ServeStaticModule.forRoot({
-//       rootPath: path.join(__dirname, '..', 'public', 'content', 'afisha'),
-//       serveRoot: '/content/afisha',
-//     }),
-//     // @todo: Добавьте раздачу статических файлов из public
-//   ],
-//   controllers: [FilmsController, OrderController],
-//   providers: [
-//     configProvider,
-//     FilmsService,
-//     OrderService,
-//     FilmsMongoDbRepository,
-//   ],
-// })
-// export class AppModule {}
--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -1,15 +1,21 @@
 import {
   BadRequestException,
+  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { PostOrderDto, PostOrdersDto } from './dto/post-order.dto';
-import { FilmsPostgresRepository } from 'src/repository/films.repository';
+import {
+  FILMS_REPOSITORY,
+  FilmsRepository,
+} from 'src/repository/films.repository.interface';
 
 @Injectable()
 export class OrderService {
-  constructor(private readonly filmsRepository: FilmsPostgresRepository) {}
+  constructor(
+    @Inject(FILMS_REPOSITORY) private readonly filmsRepository: FilmsRepository,
+  ) {}
 
   async createOrder(orders: PostOrderDto[]): Promise<PostOrdersDto> {
     const items = [];
