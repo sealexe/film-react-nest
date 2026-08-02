ab4ed85bbb470b1069d0335b1d77c2409551cc28
Ekaterina Semenova <ek.semenova94@gmail.com>
2026-08-02 16:25:53 +0300

    feat: функция-фабрика форматов логов подключена в приложение, настроена переменная LOG_FORMAT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
-import { TskvLogger } from './logger/tskv.logger';
+import { loggerFactory } from './logger/logger.factory';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -11,7 +11,7 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api/afisha');
   app.enableCors();
-  app.useLogger(new TskvLogger());
+  app.useLogger(loggerFactory(process.env.LOG_FORMAT));
   await app.listen(3000);
 }
 bootstrap();
